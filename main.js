36f80ff58e89cab7929da6d94677643675f6a458
tanuj <tanujbis10@gmail.com>
2022-01-13 19:48:54 +0530

    first commit

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ const ul = document.querySelector("ul");
 bar.addEventListener('click', () => {
     ul.classList.toggle("active");
 })
+
+
+function Circle(el)
+{
+    $(el).circleProgress({fill: {color: '#099FFF'}})
+    .on('circle-animation-progress', function(event, progress, stepValue){
+        $(this).find('strong').text(String(stepValue.toFixed(2)).substring(2)+'%');
+    });
+};
+
+Circle('.round');
+
